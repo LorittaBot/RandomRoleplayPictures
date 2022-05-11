4ec75376aa4609a7847caec57a30e0d0cbae299a
MrPowerGamerBR <git@mrpowergamerbr.com>
2022-05-11 16:46:53 -0300

    Bump version to 1.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 allprojects {
     group = "net.perfectdreams.randomroleplaypictures"
-    version = "1.0.0-SNAPSHOT"
+    version = "1.0.0"
 
     repositories {
         mavenCentral()
